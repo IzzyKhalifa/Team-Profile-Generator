3c77568b54941478c96c4d29d19c50e0e8108cca
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2022-10-18 14:50:12 +1100

    Added questions

--- index.js
+++ index.js
@@ -1,3 +1,75 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const initialQuestion = [
+  {
+    type: "input",
+    name: "mName",
+    message: "Enter your team manager's name",
+  },
+  {
+    type: "input",
+    name: "employeeID",
+    message: "Enter your employee ID",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address",
+  },
+  {
+    type: "input",
+    name: "officeNum",
+    message: "Enter your office number",
+  }
+];
+
+const menu = [
+  {
+    type: "list",
+    name: "choose",
+    message: "Choose an option",
+    choices: ["Engineer", "Intern", "Finish my team"],
+  }
+];
+
+const engineerQuest = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter engineer's name",
+  },
+  {
+    type: "input",
+    name: "ID",
+    message: "Enter engineer's ID",
+  },
+  {
+    type: "input",
+    name: "gitHub",
+    message: "Enter engineer's GitHub's username",
+  }
+];
+
+const internQuest = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter intern's name"
+    },
+    {
+        type: 'input',
+        name: 'ID',
+        message: "Enter intern's ID"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter intern's email address"
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "Enter intern's school"
+    }
+];
